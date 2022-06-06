d4f9a20c8dc5306b17590dfb1d41339a2083b777
Liu Yuchen <terrylyclow@yahoo.com>
2022-06-06 21:59:18 +0800

    Uploaded inpainting results.

--- lib/inpainting.py
+++ lib/inpainting.py
@@ -1,6 +1,7 @@
+import numpy as np
 import torch
 def inpaint(sde, data, mask, input_channels, input_height, num_steps=20, transform_forward=None, 
-             transform_reverse=None, mean=0, std=1, clip=True, epsilon=1e-3, debug=False):
+             transform_reverse=None, mean=0, std=1, clip=True, epsilon=1e-3, log_epoch=None):
     """
     The mask represents the known positions of the sample.
     """
@@ -16,10 +17,21 @@ def inpaint(sde, data, mask, input_channels, input_height, num_steps=20, transfo
     else:
         data_transformed = data
     y0 = noise * (1 - mask) + data_transformed * mask
-    y00 = y0.clone()
     ts = torch.linspace(0, 1, num_steps + 1).to(y0) * sde.T
     ones = torch.ones(y0.shape[0], 1, 1, 1).to(y0)
 
+    results = []
+
+    def output(y):
+        t = 0
+        if transform_reverse is not None:
+            t = transform_reverse(y)
+        if clip:
+            t = torch.clip(t, 0, 1)
+        return t.data.cpu().numpy()
+
+    if log_epoch is not None:
+        results.append(output(y0))
 
     with torch.no_grad():
         for i in range(num_steps):
@@ -34,19 +46,11 @@ def inpaint(sde, data, mask, input_channels, input_height, num_steps=20, transfo
             y0 = y0 + (epsilon * score / g + (2 * epsilon) ** 0.5 * torch.randn_like(y0)) # Corrector
             data_perturbed = sde.base_sde.sample(sde.T - ones * ts[i], data_transformed)
             y0 = y0 * (1. - mask) + data_perturbed * mask
+            if log_epoch is not None and (i+1) % log_epoch == 0:
+                results.append(output(y0))
             if torch.isnan(y0).any():
                 break
-
-
-    if transform_forward is not None:
-        y0 = transform_reverse(y0)
-
-    if clip:
-        y0 = torch.clip(y0, 0, 1)
-
-    # ou_sde.sample(ou_sde.T*50, x)
-
-    y0 = y0.data.cpu().numpy()
-    if debug:
-        return y0, y00
+    y0 = output(y0)
+    if log_epoch is not None:
+        return y0, np.stack(results, axis=0)
     return y0
